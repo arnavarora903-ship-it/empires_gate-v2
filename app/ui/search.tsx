d0f3a819bbf52f4045b14136caf01e8251510d9d
arnavarora903-ship-it <arnavarora903@gmail.com>
2026-02-02 17:16:51 -0500

    create invoices

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -10,9 +10,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
   
-   function handleSearch(term: string) {
-    const handleSearch = useDebouncedCallback((term) => {
+  const handleSearch = useDebouncedCallback((term) => {
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
@@ -20,7 +20,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
     replace(`${pathname}?${params.toString()}`);
     console.log(term);
-    },300);
+  }, 300);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -29,7 +30,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
-            onChange={(e) => {
+        onChange={(e) => {
           handleSearch(e.target.value);
         }}
         defaultValue={searchParams.get('query')?.toString()}
@@ -37,5 +38,5 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
   );
-}}
+}
 
